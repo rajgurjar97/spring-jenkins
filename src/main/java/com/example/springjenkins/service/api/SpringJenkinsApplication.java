38f63437ffbc92f107325cd6f793a3250a7a083c
rajgurjar97 <rajgurjarji97@gmail.com>
2020-09-01 19:39:51 +0530

    no error now i guess

--- src/main/java/com/example/springjenkins/service/api/SpringJenkinsApplication.java
+++ src/main/java/com/example/springjenkins/service/api/SpringJenkinsApplication.java
@@ -11,7 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringJenkinsApplication {
 public static	Logger logger=LoggerFactory.getLogger(SpringJenkinsApplication.class);
      @PostConstruct
-	public  intt() {
+	public void intt() {
     	logger.info("Application Started...");
     }
 	public static void main(String[] args) {
