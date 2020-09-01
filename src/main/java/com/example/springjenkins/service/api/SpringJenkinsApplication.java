4531327836d6fa28e9847fd1184e26d7f32f81bd
rajgurjar97 <rajgurjarji97@gmail.com>
2020-09-01 19:23:26 +0530

    forcefully build failed commit

--- src/main/java/com/example/springjenkins/service/api/SpringJenkinsApplication.java
+++ src/main/java/com/example/springjenkins/service/api/SpringJenkinsApplication.java
@@ -11,7 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringJenkinsApplication {
 public static	Logger logger=LoggerFactory.getLogger(SpringJenkinsApplication.class);
      @PostConstruct
-	public void intt() {
+	public  intt() {
     	logger.info("Application Started...");
     }
 	public static void main(String[] args) {
